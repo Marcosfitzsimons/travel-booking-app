54c46f7edd952fa03fffcd7b654120fc074ea569
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-02-27 10:45:00 -0300

    Change hero title

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,12 +4,14 @@ const Home = () => {
   return (
     <div className="section hero hidden">
       <div className="flex flex-col items-center gap-5 text-center">
-        <h1 className="text-6xl font-medium font-serif">
-          <span className="text-red">F</span>a
-          <span className="inline-block -rotate-12">b</span>e
-          <span className="text-red">B</span>
+        <h1 className="font-serif tracking-wider text-6xl">
+          <span className="font-medium text-[4rem]">F</span>
+          <span className="inline-block rotate-3">a</span>
+          <span className="inline-block -rotate-6">b</span>
+          <span className="inline-block rotate-1">e</span>
+          <span className="font-medium text-[4rem]">B</span>
           <span className="inline-block rotate-6">u</span>
-          <span className="inline-block -rotate-6">s</span>
+          <span className="inline-block -rotate-3">s</span>
         </h1>
         <p className="font-medium dark:text-slate-100">Carmen de Areco</p>
         <p className="font-medium dark:text-slate-100">
