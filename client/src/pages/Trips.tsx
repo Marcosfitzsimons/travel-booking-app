76189dd3da74cf08a466ead388299e3f509b91ff
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-02-27 15:42:29 -0300

    Add react-datepicker to filter trips by date

--- client/src/pages/Trips.tsx
+++ client/src/pages/Trips.tsx
@@ -1,30 +1,28 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
 import TripCard from "../components/TripCard";
-import { Button } from "../components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../components/ui/select";
 
 const Trip = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  setDefaultLocale("es");
+  registerLocale("es", es);
+
   return (
     <section className="">
       <div className="flex flex-col gap-2">
         <h2 className="text-3xl font-medium">Nuestros próximos viajes:</h2>
-        <div className="flex items-center gap-2">
+        <div className="flex flex-col gap-2">
           <p>Buscar por fecha:</p>
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="27/02/2023" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="light">26/02/2023</SelectItem>
-              <SelectItem value="dark">27/02/2023</SelectItem>
-              <SelectItem value="system">28/02/2023</SelectItem>
-            </SelectContent>
-          </Select>
+          <DatePicker
+            dateFormat={"dd/MM/yyyy"}
+            locale="es"
+            className="cursor-pointer rounded-md border bg-white/80 shadow-md border-slate-400 dark:border-slate-700 p-2 dark:text-white dark:bg-zinc-900"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
         </div>
         <div className="mt-8 flex flex-col gap-2">
           <TripCard />
