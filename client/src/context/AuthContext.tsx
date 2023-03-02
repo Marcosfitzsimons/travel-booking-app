68e5c430430a7ba6db67cf1080151900c4cb59e2
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-03-02 19:29:18 -0300

    Add auth context

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,87 @@
+import { createContext, Dispatch, useEffect, useReducer } from "react";
+/* type User = {
+  id: "";
+  username: "";
+  password: "";
+  email: "";
+  tel: "";
+  isAdmin: "";
+                  Check if this is correct.
+}; */
+interface AuthContextType {
+  user: string | null;
+  loading: boolean;
+  error: string | null;
+  dispatch?: Dispatch<AuthAction>;
+}
+
+interface AuthState {
+  user: string | null;
+  loading: boolean;
+  error: string | null;
+}
+
+type AuthAction =
+  | { type: "LOGIN_START" }
+  | { type: "LOGIN_SUCCESS"; payload: string }
+  | { type: "LOGIN_FAILURE"; payload: string }
+  | { type: "LOGOUT" };
+
+const userFromLocalStorage = localStorage.getItem("user");
+const user = userFromLocalStorage ? JSON.parse(userFromLocalStorage) : null;
+
+const INITIAL_STATE = {
+  user: user,
+  loading: false,
+  error: null,
+};
+
+export const AuthContext = createContext<AuthContextType>(INITIAL_STATE);
+
+const AuthReducer = (state: AuthState, action: AuthAction) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }: any) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
+  const value = {
+    user: state.user,
+    loading: state.loading,
+    error: state.error,
+    dispatch,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
