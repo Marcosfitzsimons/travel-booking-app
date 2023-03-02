f26f8f27ee9f3e6027251686ff0d0152acee3ad4
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-03-02 19:29:53 -0300

    add auth context provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
