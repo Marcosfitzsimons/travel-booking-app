1e5f243a56f6b5fae971ffe30b2167a8e91603eb
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-03-07 19:11:15 -0300

    Add logo component

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <Link to="/" className="font-serif tracking-wider dark:text-white">
+      <span className="font-medium text-xl">F</span>
+      <span className="inline-block rotate-3">a</span>
+      <span className="inline-block -rotate-6">b</span>
+      <span className="inline-block rotate-1">e</span>
+      <span className="font-medium text-xl">B</span>
+      <span className="inline-block rotate-6">u</span>
+      <span className="inline-block -rotate-3">s</span>
+    </Link>
+  );
+};
+
+export default Logo;
