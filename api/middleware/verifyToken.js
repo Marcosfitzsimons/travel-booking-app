e78dd8c5cfbaf5c2a8c0fc6eedb9625261ba3cb5
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-03-29 10:42:43 -0300

    Change the verify token to get the correct token

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,25 +1,23 @@
 import jwt from 'jsonwebtoken'
 import { UnauthenticatedError } from '../errors/index.js'
+import User from '../models/User.js'
 import { createError } from '../utils/error.js'
 
-export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    // const authHeader = req.headers.authorization; // That is not the token that I'm spected, I want to access the JWT that I send when user loggin 
-    // const token = authHeader.split(' ')[1] // That is not the token that I'm spected, I want to access the JWT that I send when user loggin 
-    if (!token) throw new UnauthenticatedError('No estas autorizado')
+export const verifyToken = async (req, res, next) => {
 
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1]
+    if (!token) throw new UnauthenticatedError('No estas autorizado, no token provided.')
 
-
-    jwt.verify(token, process.env.JWT, (err, user) => {
-        if (err) return next(createError(403, 'Token is not valid!'))
-        req.user = user;
-        next();
-    })
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        const decoded = jwt.verify(token, process.env.JWT)
+        req.user = await User.findById(decoded.id).select('-password')
+        next()
+    }
 }
 
 export const verifyUser = (req, res, next) => {
     verifyToken(req, res, () => {
-        console.log(req.params.id)
         if (!req.params.id) {
             return next(createError(400, "Missing id parameter"));
         }
@@ -36,7 +34,7 @@ export const verifyAdmin = (req, res, next) => {
         if (req.user.isAdmin) {
             next();
         } else {
-            throw new UnauthenticatedError('No estas autorizado')
+            throw new UnauthenticatedError('No estas autorizado, no sos admin.')
         }
     });
 };
