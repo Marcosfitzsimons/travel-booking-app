08465572df8a95de894af2d7a5079b029776f265
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-03-30 09:47:52 -0300

    Manage register errors and validation with react-hook-form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useContext, useState } from "react";
 import { motion } from "framer-motion";
 import { useToast } from "../hooks/ui/use-toast";
 import { Separator } from "../components/ui/separator";
@@ -8,12 +9,13 @@ import { Input } from "../components/ui/input";
 import axios from "axios";
 import Logo from "../components/Logo";
 import DefaultButton from "../components/DefaultButton";
+import { AuthContext } from "../context/AuthContext";
 
 type User = {
   username: string;
   fullName: string;
   email: string;
-  phone: number;
+  phone: number | null;
   image?: string;
   addressCda: string;
   addressCapital?: string;
@@ -21,37 +23,50 @@ type User = {
 };
 
 const Register = () => {
-  const [inputs, setInputs] = useState<User>({
-    username: "",
-    fullName: "",
-    password: "",
-    email: "",
-    phone: 0,
-    addressCda: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      fullName: "",
+      password: "",
+      email: "",
+      phone: null,
+      addressCda: "",
+    },
   });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
   const [err, setErr] = useState<null | string>(null);
 
   const { toast } = useToast();
   const navigate = useNavigate();
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs);
-      toast({
-        description: "Registro exitoso. Ahora podes entrar a tu cuenta.",
-      });
-      navigate("/login");
-    } catch (err: any) {
-      toast({
-        variant: "destructive",
-        description: "Registro fallido. Intentar mas tarde por favor.",
-      });
+  const handleOnSubmit = async (data: User) => {
+    if (dispatch) {
+      dispatch({ type: "LOGIN_START" });
+      try {
+        const res = await axios.post(
+          "http://localhost:8800/api/auth/register",
+          data
+        );
+        const token = res.data.token;
+        localStorage.setItem("token", token);
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        toast({
+          description: "Registro exitoso. Ahora podes reservar tu lugar.",
+        });
+        navigate("/viajes");
+      } catch (err: any) {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: err.response?.data,
+        });
+        const errorMsg = err.response.data.msg;
+        setErr(errorMsg);
+      }
     }
   };
 
@@ -99,68 +114,163 @@ const Register = () => {
             Una vez que tengas tu cuenta vas a poder reservar tu lugar.
           </p>
           <form
-            onSubmit={handleOnSubmit}
+            onSubmit={handleSubmit(handleOnSubmit)}
             className="relative w-full mt-6 p-3 py-6 flex flex-col gap-5 items-center"
           >
-            <div className="grid w-full items-center gap-3">
+            <div className="grid w-full items-center gap-2">
               <Label htmlFor="fullname">Nombre completo</Label>
               <Input
                 type="text"
                 id="fullname"
                 placeholder="Tu nombre completo"
-                name="fullName"
-                onChange={handleOnChange}
+                {...register("fullName", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu nombre completo.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Nombre y apellido no puede ser tan corto.",
+                  },
+                  maxLength: {
+                    value: 25,
+                    message: "Nombre y apellido no puede ser tan largo.",
+                  },
+                })}
               />
+              {errors.fullName && (
+                <p className="text-red-600">{errors.fullName.message}</p>
+              )}
             </div>
-            <div className="grid w-full items-center gap-3">
+            <div className="grid w-full items-center gap-2">
               <Label htmlFor="username">Nombre de usuario</Label>
               <Input
                 type="text"
                 id="username"
-                placeholder="@tomasholder666"
-                name="username"
-                onChange={handleOnChange}
+                placeholder="@username22"
+                {...register("username", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu nombre de usuario.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Nombre de usuario no puede ser tan corto.",
+                  },
+                  maxLength: {
+                    value: 15,
+                    message: "Nombre de usuario no puede ser tan largo.",
+                  },
+                })}
               />
+              {errors.username && (
+                <p className="text-red-600">{errors.username.message}</p>
+              )}
             </div>
-            <div className="grid w-full items-center gap-3">
+            <div className="grid w-full items-center gap-2">
               <Label htmlFor="password">Contraseña</Label>
               <Input
                 type="password"
                 id="password"
                 placeholder="Tu contraseña"
-                name="password"
-                onChange={handleOnChange}
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu contraseña.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Contraseña no puede ser tan corta.",
+                  },
+                  maxLength: {
+                    value: 25,
+                    message: "Contraseña no puede ser tan larga.",
+                  },
+                })}
               />
+              {errors.password && (
+                <p className="text-red-600">{errors.password.message}</p>
+              )}
             </div>
-            <div className="grid w-full items-center gap-3">
+            <div className="grid w-full items-center gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 type="email"
                 id="email"
                 placeholder="Tu email"
-                name="email"
-                onChange={handleOnChange}
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu email.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Email no puede ser tan corto.",
+                  },
+                  maxLength: {
+                    value: 40,
+                    message: "Email no puede ser tan largo.",
+                  },
+                })}
               />
+              {errors.email && (
+                <p className="text-red-600">{errors.email.message}</p>
+              )}
             </div>
-            <div className="grid w-full items-center gap-3">
-              <Label htmlFor="Celular">Celular</Label>
+            <div className="grid w-full items-center gap-2">
+              <Label htmlFor="celular">Celular</Label>
               <Input
                 type="tel"
-                id="Celular"
-                placeholder="Tu numero celular"
-                name="phone"
-                onChange={handleOnChange}
+                id="celular"
+                placeholder="Tu número celular"
+                {...register("phone", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu número celular.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Número celular no puede ser tan corto.",
+                  },
+                  maxLength: {
+                    value: 25,
+                    message: "Número celular no puede ser tan largo.",
+                  },
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Número celular debe incluir solo números.",
+                  },
+                })}
               />
+              {errors.phone && (
+                <p className="text-red-600">{errors.phone.message}</p>
+              )}
             </div>
-            <div className="grid w-full items-center gap-3">
+            <div className="grid w-full items-center gap-2">
               <Label htmlFor="address">Domicilio (Carmen)</Label>
               <Input
                 id="address"
                 placeholder="Tu domicilio en Carmen"
-                name="addressCda"
-                onChange={handleOnChange}
+                {...register("addressCda", {
+                  required: {
+                    value: true,
+                    message: "Por favor, ingresa tu domicilio.",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Domicilio no puede ser tan corto.",
+                  },
+                  maxLength: {
+                    value: 25,
+                    message: "Domicilio no puede ser tan largo.",
+                  },
+                })}
               />
+              {errors.addressCda && (
+                <p className="text-red-600">{errors.addressCda.message}</p>
+              )}
             </div>
+            {err && <p className="text-red-600 self-start">{err}</p>}
             <DefaultButton>Crear cuenta</DefaultButton>
             <p className="lg:self-start">
               ¿Ya tenes cuenta?{" "}
