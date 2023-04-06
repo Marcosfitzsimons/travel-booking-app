a2fee58f4f55876234e83f0667c234f0362a1233
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-06 16:38:57 -0300

    Add new pages

---
+++ dashboard/src/pages/List.tsx
@@ -0,0 +1,7 @@
+import Datatable from "../components/Datatable";
+
+const List = () => {
+  return <Datatable />;
+};
+
+export default List;
---
+++ dashboard/src/pages/New.tsx
@@ -0,0 +1,6 @@
+const New = () => {
+  // new user or new trip
+  return <div>New</div>;
+};
+
+export default New;
