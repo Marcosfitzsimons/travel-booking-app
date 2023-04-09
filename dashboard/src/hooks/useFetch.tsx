faa8dee143bef1c1f4f11e34017e451798b6757c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-09 11:32:35 -0300

    Add useFetch hook

---
+++ dashboard/src/hooks/useFetch.tsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<unknown | boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const token = localStorage.getItem("token");
+        const headers = {
+          Authorization: `Bearer ${token}`,
+        };
+        const res = await axios.get(url, { headers });
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
