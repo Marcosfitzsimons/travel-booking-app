8ed6e6ef477336c4b02028c4ba6c9698ad4bc792
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-12 10:31:57 -0300

    Add outline colros

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 
 const BackButton = ({ toProfile }: ButtonProps) => {
   return (
-    <div className="relative after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] dark:after:shadow-highlight dark:after:shadow-blue-lagoon-300/60 focus-within:after:shadow-[#77f6aa] after:transition dark:after:hover:shadow-blue-lagoon-300/80">
+    <div className="relative after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] dark:after:shadow-highlight dark:after:shadow-blue-lagoon-300/60 after:transition dark:after:hover:shadow-blue-lagoon-300/80  focus-within:after:shadow-blue-lagoon-200 dark:focus-within:after:shadow-blue-lagoon-200">
       <Button className="relative flex items-center h-8 rounded-lg shadow-sm shadow-blue-lagoon-900/30 bg-white border border-blue-lagoon-200 hover:bg-white hover:border-blue-lagoon-600/50 dark:bg-black dark:text-blue-lagoon-100 dark:shadow-input dark:shadow-black/5 dark:border-none dark:hover:text-white">
         <ChevronsLeft className="mr-1 w-5 aspect-square" />
         <Link to={toProfile ? "/mi-perfil" : "/viajes"}>Volver</Link>
--- client/src/components/DefaultButton.tsx
+++ client/src/components/DefaultButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const DefaultButton = ({ children }: Props) => {
   return (
-    <div className="relative w-full after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/20 dark:after:shadow-highlight dark:after:shadow-blue-lagoon-100/20 focus-within:after:shadow-[#77f6aa] after:transition">
+    <div className="relative w-full after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/20 dark:after:shadow-highlight dark:after:shadow-blue-lagoon-100/20  after:transition focus-within:after:shadow-blue-lagoon-200 dark:focus-within:after:shadow-blue-lagoon-200">
       <Button className="relative w-full bg-blue-lagoon-500 text-slate-100  hover:text-white dark:shadow-input dark:shadow-black/5 dark:text-slate-100 dark:hover:text-white dark:bg-blue-lagoon-500">
         {children}
       </Button>
