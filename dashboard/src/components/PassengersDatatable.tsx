1c939035d46f5fafa248e5cd56bb69de084b6fcc
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-17 15:06:57 -0300

    Add passengers datatable

---
+++ dashboard/src/components/PassengersDatatable.tsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Trash2 } from "lucide-react";
+import axios from "axios";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { toast } from "../hooks/ui/use-toast";
+import { Link } from "react-router-dom";
+
+type Passenger = {
+  _id: string;
+  createdBy: UserData;
+};
+
+type Trip = {
+  _id: string;
+  name: string;
+  date: string;
+  from: string;
+  to: string;
+  departureTime: string;
+  arrivalTime: string;
+  maxCapacity: number | undefined;
+  price: number | undefined;
+  available: boolean;
+  passengers: Passenger[];
+};
+
+type UserData = {
+  _id: string;
+  addressCapital: string;
+  addressCda: string;
+  email: string;
+  fullName: string;
+  image?: string;
+  myTrips: TripProps[];
+  phone: undefined | number;
+  username: string;
+};
+
+interface Column {
+  field: string;
+  headerName: string;
+  width: number;
+  renderCell?: (params: any) => JSX.Element;
+}
+
+type DataTableProps = {
+  columns: Column[];
+  tripData: Trip;
+  tripPassengers: Passenger[];
+};
+
+const PassengersDatable = ({
+  columns,
+  tripPassengers,
+  tripData,
+}: DataTableProps) => {
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState<null | string>(null);
+  const [list, setList] = useState(tripPassengers);
+
+  // get the id of the user
+  const userId = "123135345634634534";
+  const token = localStorage.getItem("token");
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const handleDelete = async (tripId: string) => {
+    setLoading(true);
+    try {
+      await axios.delete(
+        `https://travel-booking-api-production.up.railway.app/api/passengers/${userId}/${tripId}`,
+        { headers }
+      );
+      toast({
+        description: "Lugar cancelado con éxito.",
+      });
+      setLoading(false);
+      setList(list.filter((item) => item.id !== tripId));
+    } catch (err: any) {
+      console.log(err);
+      setLoading(false);
+      setErr(err.message);
+      toast({
+        variant: "destructive",
+        description: `Error al cancelar lugar, intente más tarde. ${
+          err ? `"${err}"` : ""
+        }`,
+      });
+    }
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Acción",
+      width: 180,
+      renderCell: (params: any) => {
+        return (
+          <div className="flex items-center gap-2">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <div className="relative flex items-center">
+                  <Trash2 className="absolute cursor-pointer text-white left-2 h-4 w-4" />
+                  <button
+                    className={`px-2 pl-7 text-white font-medium rounded-md border border-red-500 bg-red-500 hover:border-blue-lagoon-600/50 dark:border-red-600 dark:bg-red-600 dark:hover:border-blue-lagoon-300/80`}
+                  >
+                    Borrar
+                  </button>
+                </div>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>¿Estás seguro?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Esta acción no podrá deshacerse. Esto eliminará
+                    permanentemente al usuario del viaje.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter className="flex flex-col-reverse gap-1 md:flex-row md:justify-end">
+                  <AlertDialogCancel className="md:w-auto">
+                    No, volver al perfil del usuario.
+                  </AlertDialogCancel>
+                  <AlertDialogAction
+                    onClick={() => handleDelete(params.row.id)}
+                    className="md:w-auto"
+                  >
+                    Si, borrar pasajero
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="h-[400px] w-full">
+      <DataGrid
+        rows={list}
+        columns={actionColumn.concat(columns)}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 9,
+            },
+          },
+        }}
+        pageSizeOptions={[9]}
+        checkboxSelection
+        getRowId={(row) => row._id}
+        className="w-[min(100%,1000px)] text-blue-lagoon-800 bg-white/40 border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-[#141414] dark:text-neutral-100"
+      />
+    </div>
+  );
+};
+
+export default PassengersDatable;
