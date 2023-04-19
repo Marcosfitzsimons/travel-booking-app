8247ba46575bfd54c5c2a88e4a8a3aea1d30b063
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-19 11:46:57 -0300

    Some change

--- dashboard/src/pages/SingleUser.tsx
+++ dashboard/src/pages/SingleUser.tsx
@@ -209,7 +209,7 @@ const SingleUser = () => {
             </ul>
             <Dialog>
               <div className="relative w-full after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/20 dark:after:shadow-highlight dark:after:shadow-blue-lagoon-100/20 after:transition focus-within:after:shadow-blue-lagoon-200 dark:focus-within:after:shadow-blue-lagoon-200 lg:max-w-[9rem] lg:h-8">
-                <DialogTrigger className="relative w-full bg-blue-lagoon-500 text-slate-100 rounded-lg  hover:text-white dark:shadow-input dark:shadow-black/5 dark:text-slate-100 dark:hover:text-white dark:bg-blue-lagoon-500 lg:max-w-[9rem] lg:h-8">
+                <DialogTrigger className="relative w-full bg-blue-lagoon-500 text-slate-100 rounded-lg  hover:text-white dark:shadow-input dark:shadow-black/5 dark:text-slate-100 dark:hover:text-white dark:bg-blue-lagoon-500 lg:max-w-[9rem] h-8">
                   Editar
                 </DialogTrigger>
               </div>
