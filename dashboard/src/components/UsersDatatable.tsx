898947ee2ce3050ef0adeefd082f88677f653238
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-20 09:58:34 -0300

    Change users datatable

--- dashboard/src/components/UsersDatatable.tsx
+++ dashboard/src/components/UsersDatatable.tsx
@@ -1,20 +1,21 @@
 import { DataGrid } from "@mui/x-data-grid";
-import { useNavigate } from "react-router-dom";
-import { Mail, MapPin, Phone, Plus, User } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+import { Eye, PlusCircle, Trash2, UserPlus } from "lucide-react";
 import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import axios from "axios";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import DefaultButton from "./DefaultButton";
-import { toast } from "../hooks/ui/use-toast";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import SearchUserInput from "./SearchUserInput";
 
 interface Column {
   field: string;
@@ -23,142 +24,88 @@ interface Column {
   renderCell?: (params: any) => JSX.Element;
 }
 
-type UserDataTableProps = {
+type DataTableProps = {
   columns: Column[];
-  tripId: string | undefined;
+  linkText: string;
 };
 
-const UserDatatable = ({ columns, tripId }: UserDataTableProps) => {
-  const [loading, setLoading] = useState(false);
-  const [err, setErr] = useState<unknown | boolean>(false);
+const UsersDatatable = ({ columns, linkText }: DataTableProps) => {
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
   const [list, setList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
-  const navigate = useNavigate();
+  const baseUrl = `https://travel-booking-api-production.up.railway.app/api/${path}`;
 
-  const baseUrl = `https://travel-booking-api-production.up.railway.app/api/users`;
-
-  const { data, error } = useFetch(baseUrl);
+  const { data, loading, error } = useFetch(baseUrl);
 
   const token = localStorage.getItem("token");
   const headers = {
     Authorization: `Bearer ${token}`,
   };
 
-  const handleAddPassenger = async (userId: string) => {
-    setLoading(true);
+  const handleDelete = async (id: string) => {
     try {
-      await axios.post(
-        `https://travel-booking-api-production.up.railway.app/api/passengers/${userId}/${tripId}`,
-        {},
-        { headers }
+      await axios.delete(
+        `https://travel-booking-api-production.up.railway.app/api/${path}/${id}`,
+        {
+          headers,
+        }
       );
-      setLoading(false);
-      toast({
-        description: "Pasajero agregado con éxito.",
-      });
-      setTimeout(() => {
-        navigate(`/trips/${tripId}`);
-      }, 1000);
-    } catch (err: any) {
-      const errorMsg = err.response.data.msg;
-      setLoading(false);
-      setErr(errorMsg);
-      toast({
-        variant: "destructive",
-        description: `Error al agregar pasajero, intentar más tarde. ${errorMsg}`,
-      });
-    }
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {}
   };
 
   const actionColumn = [
     {
       field: "action",
       headerName: "Acción",
-      width: 120,
+      width: 180,
       renderCell: (params) => {
         return (
           <div className="flex items-center gap-2">
-            <Dialog>
-              <DialogTrigger asChild>
+            <div className="relative flex items-center">
+              <Eye className="absolute cursor-pointer left-2 h-4 w-4" />
+              <Link
+                to={`/${path}/${params.row._id}`}
+                className={`px-3 pl-7 rounded-md border border-blue-lagoon-200 bg-white hover:border-blue-lagoon-600/50 dark:border-blue-lagoon-300/60 dark:text-blue-lagoon-100 dark:bg-black dark:hover:border-blue-lagoon-300/80`}
+              >
+                Ver
+              </Link>
+            </div>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
                 <div className="relative flex items-center">
-                  <Plus className="absolute cursor-pointer left-2 h-4 w-4" />
+                  <Trash2 className="absolute cursor-pointer left-2 h-4 w-4" />
                   <button
-                    className={`px-3 pl-7 rounded-md border border-blue-lagoon-200 bg-white hover:border-blue-lagoon-600/50 dark:border-blue-lagoon-300/60 dark:text-blue-lagoon-100 dark:bg-black dark:hover:border-blue-lagoon-300/80`}
+                    className={`px-2 pl-7 rounded-md border border-red-500 bg-red-500 hover:border-blue-lagoon-600/50 dark:border-red-600 dark:bg-red-600 dark:hover:border-blue-lagoon-300/80`}
                   >
-                    Agregar
+                    Borrar
                   </button>
                 </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle className="text-center">
-                    Agregar pasajero al viaje
-                  </DialogTitle>
-                </DialogHeader>
-                <div className="mt-2">
-                  <div className="flex flex-col items-center gap-3">
-                    <div className="w-full relative flex flex-col items-center lg:basis-1/3">
-                      <Avatar className="w-32 h-32">
-                        <AvatarImage
-                          className="origin-center hover:origin-bottom hover:scale-105 transition-all duration-200 z-90 align-middle"
-                          src={params.row.image}
-                          alt="avatar"
-                        />
-                        <AvatarFallback>
-                          <User className="w-12 h-12" />
-                        </AvatarFallback>
-                      </Avatar>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <h3 className="font-medium text-xl dark:text-white ">
-                        {params.row.fullName}
-                      </h3>
-                      <h4 className="text-[#737373]">@{params.row.username}</h4>
-                    </div>
-                    <div className="w-full flex flex-col items-center gap-5">
-                      <ul className="flex flex-col w-full overflow-hidden bg-white gap-2 border border-blue-lagoon-500/20 items-start p-4 shadow-inner rounded-md dark:bg-black dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300">
-                        <li className="flex items-center gap-1">
-                          <Mail className="h-4 w-4" />
-                          <span className="font-medium">Email:</span>
-                          {params.row.email}
-                        </li>
-                        <li className="flex items-center gap-1">
-                          <Phone className="h-4 w-4" />
-                          <span className="font-medium">Celular:</span>{" "}
-                          {params.row.phone}
-                        </li>
-
-                        <li className="flex items-center gap-1 shrink-0">
-                          <MapPin className="w-4 h-4 shrink-0" />
-                          <span className="font-medium shrink-0">
-                            Dirrección Carmen:
-                          </span>
-                          <span className="shrink-0">
-                            {params.row.addressCda}
-                          </span>
-                        </li>
-
-                        <li className="flex items-center gap-1 shrink-0">
-                          <MapPin className="w-4 h-4 shrink-0" />
-                          <span className="font-medium shrink-0">
-                            Dirrecion Capital:
-                          </span>
-                          <span className="shrink-0">
-                            {params.row.addressCapital}
-                          </span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div
-                    className="flex justify-center my-4"
-                    onClick={() => handleAddPassenger(params.row._id)}
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>¿Estás seguro?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Esta acción no podrá deshacerse. Esto eliminará
+                    permanentemente el {path === "users" ? "usuario" : "viaje"}
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter className="flex flex-col-reverse gap-1 md:flex-row md:justify-end">
+                  <AlertDialogCancel className="md:w-auto">
+                    No, volver al listado de{" "}
+                    {path === "users" ? "usuarios" : "viajes"}
+                  </AlertDialogCancel>
+                  <AlertDialogAction
+                    onClick={() => handleDelete(params.row._id)}
+                    className="md:w-auto"
                   >
-                    <DefaultButton>Agregar pasajero</DefaultButton>
-                  </div>
-                </div>
-              </DialogContent>
-            </Dialog>
+                    Si, borrar {path === "users" ? "usuario" : "viaje"}
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
         );
       },
@@ -171,23 +118,59 @@ const UserDatatable = ({ columns, tripId }: UserDataTableProps) => {
 
   return (
     <div className="h-[400px] w-full">
-      <DataGrid
-        rows={list}
-        columns={actionColumn.concat(columns)}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 9,
+      <div className="w-full my-3 flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
+        <SearchUserInput list={list} setFilteredList={setFilteredList} />
+        <div className="relative md:self-end md:my-1">
+          {path === "users" ? (
+            <UserPlus className="absolute cursor-pointer left-3 h-5 w-5" />
+          ) : (
+            <PlusCircle className="absolute cursor-pointer left-3 top-[4px] h-4 w-4" />
+          )}
+          <Link
+            to={`/${path}/new`}
+            className={`px-3 py-1 ${
+              path === "users" ? "pl-9" : "pl-8"
+            } rounded-md border border-blue-lagoon-200 shadow-md bg-white hover:border-blue-lagoon-600/50 dark:border-blue-lagoon-300/60 dark:text-blue-lagoon-100 dark:bg-[#141414] dark:hover:border-blue-lagoon-300/80`}
+          >
+            {linkText}
+          </Link>
+        </div>
+      </div>
+      {filteredList.length > 0 ? (
+        <DataGrid
+          rows={filteredList}
+          columns={actionColumn.concat(columns)}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 9,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[9]}
-        checkboxSelection
-        getRowId={(row) => row._id}
-        className="w-[min(100%,1000px)] text-blue-lagoon-800 bg-white/40 shadow-md border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-[#141414] dark:text-neutral-100"
-      />
+          }}
+          pageSizeOptions={[9]}
+          checkboxSelection
+          getRowId={(row) => row._id}
+          className="w-[min(100%,1000px)] text-blue-lagoon-800 bg-white/40 shadow-md border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-[#141414] dark:text-neutral-100"
+        />
+      ) : (
+        <DataGrid
+          rows={list}
+          columns={actionColumn.concat(columns)}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 9,
+              },
+            },
+          }}
+          pageSizeOptions={[9]}
+          checkboxSelection
+          getRowId={(row) => row._id}
+          className="w-[min(100%,1000px)] text-blue-lagoon-800 bg-white/40 shadow-md border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-[#141414] dark:text-neutral-100"
+        />
+      )}
     </div>
   );
 };
 
-export default UserDatatable;
+export default UsersDatatable;
