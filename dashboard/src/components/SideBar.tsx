73203bde74035b7816b20e7a9a005af7d3bad6fa
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-20 16:11:05 -0300

    Add hover states

--- dashboard/src/components/SideBar.tsx
+++ dashboard/src/components/SideBar.tsx
@@ -23,33 +23,42 @@ const SideBar = () => {
     <div className=""></div>
   ) : (
     <div className="hidden flex-[1] h-screen dark:text-blue-lagoon-100 lg:flex lg:flex-col lg:items-end lg:py-[200px] lg:pr-10">
-      <div className="w-full max-w-[12rem] rounded-md flex flex-col mx-auto shadow-md gap-5 p-5 mt-6 bg-white/40 border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-[#141414]">
+      <div className="w-full max-w-[12rem] rounded-md flex flex-col mx-auto shadow-md gap-5 p-5 mt-6 bg-white/40 border border-blue-lagoon-500/20 dark:border-blue-lagoon-300/60 dark:hover:border-blue-lagoon-300 dark:bg-black">
         <nav className="">
-          <ul className="flex flex-col gap-1">
-            <p className="text-blue-lagoon-800/30 uppercase font-bold text-sm dark:text-white">
+          <ul className="flex flex-col">
+            <p className="text-blue-lagoon-900 uppercase pb-1 font-bold text-sm dark:text-white">
               Listas
             </p>
             <li className="relative flex items-center gap-2">
               <Map className="absolute left-2 h-5 w-5" />
-              <Link to="/trips" className="z-20 w-full pl-8">
+              <Link
+                to="/trips"
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-100 dark:hover:text-white dark:hover:bg-blue-lagoon-900/50"
+              >
                 Viajes
               </Link>
             </li>
             <li className="relative flex items-center gap-2">
               <User className="absolute left-2 h-5 w-5" />
-              <Link to="/users" className="z-20 w-full pl-8">
+              <Link
+                to="/users"
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-100 dark:hover:text-white dark:hover:bg-blue-lagoon-900/50"
+              >
                 Usuarios
               </Link>
             </li>
           </ul>
         </nav>
-        <ul className="flex flex-col gap-1">
-          <p className="text-blue-lagoon-800/30 uppercase font-bold text-sm dark:text-white">
+        <ul className="flex flex-col">
+          <p className="text-blue-lagoon-900 uppercase pb-1 font-bold text-sm dark:text-white">
             Admin
           </p>
           <li className="relative flex items-center gap-2">
             <User className="absolute left-2 h-5 w-5" />
-            <Link to="/mi-perfil" className="w-full pl-8 z-20">
+            <Link
+              to="/mi-perfil"
+              className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-100 dark:hover:text-white dark:hover:bg-blue-lagoon-900/50"
+            >
               Perfil
             </Link>
           </li>
@@ -58,7 +67,7 @@ const SideBar = () => {
             <Link
               to="/login"
               onClick={handleLogOut}
-              className="w-full pl-8 z-20"
+              className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-100 dark:hover:text-white dark:hover:bg-blue-lagoon-900/50"
             >
               Salir
             </Link>
