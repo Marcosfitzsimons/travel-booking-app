06f913a7acb0436d678ec1314155b47335549de3
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-24 18:51:48 -0300

    Change width to container element

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   };
 
   return (
-    <div className="App flex w-[min(90%,1200px)] mx-auto">
+    <div className="App flex w-[min(90%,1300px)] mx-auto">
       <SideBar />
       <div className="flex-[6] w-[min(100%,1000px)]">
         <Header />
