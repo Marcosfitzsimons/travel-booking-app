fc22023ffc6476664d2db24b6fb1461d4d1d609e
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-04-27 10:43:13 -0300

    Add vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { inject } from "@vercel/analytics";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { inject } from "@vercel/analytics";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
