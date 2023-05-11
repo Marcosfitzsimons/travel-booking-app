8bed7ce54370ad8cc1106e0db8608a33df1d6a2a
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-05-11 17:28:43 -0300

    Add avatar component

--- client/src/components/PublicationCard.tsx
+++ client/src/components/PublicationCard.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Logo from "./Logo";
+import logo from "../assets/fabebus-logo.jpg";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const PublicationCard = () => {
   return (
     <article className="w-full relative rounded-md border border-border-color bg-white/80 p-4 py-6 flex flex-col gap-3 dark:border-border-color-dark dark:bg-black/60">
       <div className="flex items-center gap-2">
-        <Logo />
+        <Avatar className="w-8 h-8">
+          <AvatarImage src={logo} alt="fabebus" />
+          <AvatarFallback>Fabebus</AvatarFallback>
+        </Avatar>
         <h3>Nombre publicación</h3>
       </div>
       <div className="">
