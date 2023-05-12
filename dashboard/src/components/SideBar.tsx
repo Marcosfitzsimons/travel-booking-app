63f0c701c2677b48f26e7b9e53c900881ab69e37
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-05-12 07:07:18 -0300

    Change users icon

--- dashboard/src/components/SideBar.tsx
+++ dashboard/src/components/SideBar.tsx
@@ -2,6 +2,7 @@ import { User, Map, LogOut } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Users } from "lucide-react";
 
 const SideBar = () => {
   const { user, dispatch } = useContext(AuthContext);
@@ -39,7 +40,7 @@ const SideBar = () => {
               </Link>
             </li>
             <li className="relative flex items-center gap-2">
-              <User className="absolute left-2 h-5 w-5" />
+              <Users className="absolute left-2 h-5 w-5" />
               <Link
                 to="/users"
                 className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-100 dark:hover:text-white dark:hover:bg-blue-lagoon-900/50"
