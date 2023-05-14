514cd71f48f046eca4f88d8e23e31c4f35e4ccdd
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-05-14 17:03:39 -0300

    Some change

--- client/src/components/SectionTitle.tsx
+++ client/src/components/SectionTitle.tsx
@@ -4,7 +4,7 @@ interface MyComponentProps {
 
 const SectionTitle = ({ children }: MyComponentProps) => {
   return (
-    <h3 className="relative text-3xl font-medium  py-6 mb-6 dark:text-white lg:text-4xl">
+    <h3 className="relative text-3xl font-medium py-6 mb-6 dark:text-white lg:text-4xl">
       {children}
       <span className="absolute bottom-4 left-0 bg-red-700 h-1 w-20"></span>
       <span className="absolute bottom-2 left-0 bg-red-700 h-1 w-10"></span>
