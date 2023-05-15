6a715f6073a213608d5fb82d6682e5bfa8ec6fd5
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-05-15 11:34:31 -0300

    Change text color

--- client/src/components/DefaultButton.tsx
+++ client/src/components/DefaultButton.tsx
@@ -11,7 +11,7 @@ const DefaultButton = ({ children, loading, isMaxCapacity }: Props) => {
     <div className="relative w-full after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/20 dark:after:shadow-highlight dark:after:shadow-blue-lagoon-100/20 after:transition focus-within:after:shadow-blue-lagoon-200 dark:focus-within:after:shadow-blue-lagoon-200 lg:h-8">
       <Button
         disabled={isMaxCapacity || loading}
-        className="relative w-full bg-[#9e4a4f] text-slate-100 hover:text-white dark:shadow-input dark:shadow-black/5 dark:text-slate-100 dark:hover:text-white dark:bg-[#9e4a4f] lg:h-8"
+        className="relative w-full bg-[#9e4a4f] text-blue-lagoon-100 hover:text-white dark:shadow-input dark:shadow-black/5 dark:text-blue-lagoon-100 dark:hover:text-white dark:bg-[#9e4a4f] lg:h-8"
       >
         {children}
       </Button>
