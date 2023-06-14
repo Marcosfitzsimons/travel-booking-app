532854d2ccaa48cec0a67c3952e8f90e44cfcd2d
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-14 12:02:50 -0300

    Update design on large screen

--- client/src/components/UserInfo.tsx
+++ client/src/components/UserInfo.tsx
@@ -2,6 +2,7 @@ import {
   Contact,
   ContactIcon,
   Crop,
+  Fingerprint,
   Mail,
   MapPin,
   Milestone,
@@ -85,7 +86,7 @@ const UserInfo = ({ userData }: UserInfoProps) => {
         initial="hidden"
         animate="visible"
         exit="exit"
-        className="w-full relative flex flex-col items-center gap-5 md:w-7/12 md:mx-auto"
+        className="w-full relative flex flex-col items-center gap-5 md:w-11/12 md:mx-auto"
       >
         <Avatar className="w-32 h-32">
           <AvatarImage
@@ -105,68 +106,81 @@ const UserInfo = ({ userData }: UserInfoProps) => {
           <h4 className="text-[#737373] dark:text-slate-500">
             @{userData?.username}
           </h4>
+          <Separator className="hidden w-8 self-center bg-border-color mt-7 lg:flex dark:bg-border-color-dark" />
         </div>
-        <div className="flex flex-col w-full overflow-hidden gap-2 max-w-sm border border-border-color items-start p-4 bg-white shadow-inner rounded-md dark:bg-black/60 dark:border-zinc-500">
-          <h5 className="text-center w-full font-medium dark:text-white">
-            Mis datos
-          </h5>
-          <ul className="flex flex-col w-full overflow-hidden gap-2   ">
-            <li className="flex items-center gap-1">
-              <Mail className="h-4 w-4 text-icon-color dark:text-icon-color-dark" />
-              <span className="font-medium">Email:</span>
-              {userData?.email}
-            </li>
-            <li className="flex items-center gap-1">
-              <Phone className="h-4 w-4 text-icon-color dark:text-icon-color-dark" />
-              <span className="font-medium">Celular:</span> {userData?.phone}
-            </li>
-            <li className="flex items-center gap-1 shrink-0">
-              <ContactIcon className="w-4 h-4 text-icon-color dark:text-icon-color-dark shrink-0" />
-              <span className="font-medium shrink-0">DNI:</span>
-              <span className="shrink-0">{userData?.dni}</span>
-            </li>
-            <li className="flex items-center gap-1 shrink-0">
-              <MapPin className="w-4 h-4 text-icon-color dark:text-icon-color-dark shrink-0" />
-              <span className="font-medium shrink-0">Dirreción (Carmen):</span>
-              <span className="shrink-0">{userData?.addressCda.street}</span>
-            </li>
-            <li className="flex items-center gap-1 shrink-0">
-              <MapPin className="w-4 h-4 text-icon-color  dark:text-icon-color-dark shrink-0" />
-              <span className="font-medium shrink-0">Dirreción (Capital):</span>
-              <span className="shrink-0">{userData?.addressCapital}</span>
-            </li>
-          </ul>
-          <Separator className="w-8 self-center mt-2 bg-border-color lg:hidden dark:bg-border-color-dark" />
-          <div className="flex flex-col gap-1">
-            <h5 className="text-center w-full font-medium dark:text-white">
+        <div className="flex flex-col w-full overflow-hidden gap-2 max-w-sm items-start px-2 lg:px-0 lg:flex-row lg:pt-0 lg:justify-around lg:max-w-6xl">
+          <div className="w-full flex flex-col gap-1 lg:basis-1/3 lg:my-2">
+            <Separator className="w-8 self-center my-2 bg-border-color lg:hidden dark:bg-border-color-dark" />
+            <h5 className="text-center w-full font-medium dark:text-white lg:mb-2 lg:text-xl">
+              Datos personales
+            </h5>
+
+            <ul className="flex flex-col w-full overflow-hidden gap-1 lg:shadow-md lg:py-2 lg:px-4 lg:rounded-md lg:bg-white lg:border lg:border-border-color lg:dark:border-border-color-dark lg:dark:bg-black">
+              <li className="flex items-center gap-1">
+                <Mail className="h-4 w-4 text-icon-color shrink-0 dark:text-icon-color-dark" />
+                <span className="font-medium">Email:</span>
+                {userData?.email}
+              </li>
+              <li className="flex items-center gap-1">
+                <Phone className="h-4 w-4 text-icon-color dark:text-icon-color-dark" />
+                <span className="font-medium">Celular:</span> {userData?.phone}
+              </li>
+              <li className="flex items-center gap-1 shrink-0">
+                <Fingerprint className="w-4 h-4 text-icon-color dark:text-icon-color-dark shrink-0" />
+                <span className="font-medium shrink-0">DNI:</span>
+                <span className="shrink-0">{userData?.dni}</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="w-full flex flex-col gap-1 lg:basis-[60%] lg:my-2">
+            <Separator className="w-8 self-center my-2 bg-border-color lg:hidden dark:bg-border-color-dark" />
+            <h5 className="text-center w-full font-medium dark:text-white lg:mb-2 lg:text-xl">
               Domicilios
             </h5>
-            <div className="flex flex-col gap-1">
-              <h6 className="font-serif dark:text-white">Carmen de Areco:</h6>
-              <div className="flex items-center gap-[2px] ">
-                <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
-                <span className="font-medium dark:text-white">Dirreción:</span>
-                <p>{`${userData?.addressCda.street} ${userData?.addressCda.streetNumber}`}</p>
+            <div className="flex flex-col gap-1 lg:shadow-md lg:flex-row lg:justify-between lg:py-2 lg:px-4 lg:rounded-md lg:bg-white lg:border lg:border-border-color lg:dark:border-border-color-dark lg:dark:bg-black">
+              <div className="flex flex-col gap-1 lg:basis-[55%]">
+                <h6 className="font-serif text-icon-color dark:text-icon-color-dark">
+                  Carmen de Areco
+                </h6>
+                <div className="flex items-center gap-1">
+                  <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                  <span className="font-medium dark:text-white">
+                    Dirreción:
+                  </span>
+                  <p>{`${userData?.addressCda.street} ${userData?.addressCda.streetNumber}`}</p>
+                </div>
+                <div className="flex flex-col gap-[2px] sm:flex-row sm:items-center sm:gap-1">
+                  <div className="flex items-center gap-1">
+                    <Crop className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                    <span className="font-medium dark:text-white">
+                      Calles que cruzan:
+                    </span>
+                  </div>
+                  <span className="">{userData?.addressCda.crossStreets}</span>
+                </div>
               </div>
 
-              <div className="flex items-center gap-[2px]">
-                <Crop className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
-                <span className="font-medium dark:text-white">
-                  Calles que cruzan:
-                </span>{" "}
-                {userData?.addressCda.crossStreets}
-              </div>
-              <h6 className="font-serif dark:text-white">Capital Federal:</h6>
-              <div className="flex items-center gap-[2px]">
-                <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
-                <span className="font-medium dark:text-white">Dirreción:</span>{" "}
-                <p>{userData?.addressCapital}</p>
+              <div className="flex flex-col gap-1 lg:basis-[40%]">
+                <h6 className="font-serif text-icon-color dark:text-icon-color-dark">
+                  Capital Federal
+                </h6>
+                <div className="flex items-center gap-1">
+                  <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                  <span className="font-medium dark:text-white">
+                    Dirreción:
+                  </span>{" "}
+                  <p>{userData?.addressCapital}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="w-full max-w-sm lg:w-[10rem]" onClick={goToEditProfile}>
+        <div
+          className="px-2 w-full max-w-sm lg:w-[10rem]"
+          onClick={goToEditProfile}
+        >
           <DefaultButton>Editar perfil</DefaultButton>
         </div>
       </motion.div>
