3270ac5743098855e351128aea1fe9f8c858a56c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-15 12:20:13 -0300

    Update user properties

--- dashboard/src/components/NewPassengerDatatable.tsx
+++ dashboard/src/components/NewPassengerDatatable.tsx
@@ -1,6 +1,15 @@
 import { DataGrid } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
-import { Mail, MapPin, Phone, Plus, User } from "lucide-react";
+import {
+  Crop,
+  Fingerprint,
+  Mail,
+  MapPin,
+  Milestone,
+  Phone,
+  Plus,
+  User,
+} from "lucide-react";
 import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import axios from "axios";
@@ -17,6 +26,7 @@ import DefaultButton from "./DefaultButton";
 import { toast } from "../hooks/ui/use-toast";
 import SearchUserInput from "./SearchUserInput";
 import { ContactIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
 
 type UserDataTableProps = {
   columns: any;
@@ -93,66 +103,100 @@ const NewPassengerDatatable = ({ columns, tripId }: UserDataTableProps) => {
                     Agregar pasajero al viaje
                   </DialogTitle>
                 </DialogHeader>
-                <div className="mt-2">
-                  <div className="flex flex-col items-center gap-3">
-                    <div className="w-full relative flex flex-col items-center lg:basis-1/3">
-                      <Avatar className="w-32 h-32">
-                        <AvatarImage
-                          className="origin-center hover:origin-bottom hover:scale-105 transition-all duration-200 z-90 align-middle"
-                          src={params.row.image}
-                          alt="avatar"
-                        />
-                        <AvatarFallback>
-                          <User className="w-12 h-12" />
-                        </AvatarFallback>
-                      </Avatar>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <h3 className="font-medium text-xl dark:text-white ">
-                        {params.row.fullName}
-                      </h3>
-                      <h4 className="text-[#737373]">@{params.row.username}</h4>
-                    </div>
-                    <div className="w-full flex flex-col items-center gap-5">
-                      <ul className="flex flex-col w-full overflow-hidden bg-white gap-2 border border-border-color items-start p-4 shadow-inner rounded-md dark:bg-black/80 dark:border-border-color-dark dark:hover:border-blue-lagoon-300">
-                        <li className="flex items-center gap-1 shrink-0">
-                          <MapPin className="h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300 shrink-0" />
-                          <span className="font-medium shrink-0">
-                            Dirrección Carmen:
-                          </span>
-                          <span className="shrink-0">
-                            {params.row.addressCda}
-                          </span>
-                        </li>
-                        <li className="flex items-center gap-1 shrink-0">
-                          <MapPin className="h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300 shrink-0" />
-                          <span className="font-medium shrink-0">
-                            Dirrecion Capital:
-                          </span>
-                          <span className="shrink-0">
-                            {params.row.addressCapital}
-                          </span>
+                <div className="mt-2 flex flex-col items-center">
+                  <Avatar className="w-32 h-32">
+                    <AvatarImage
+                      className="origin-center hover:origin-bottom hover:scale-105 transition-all duration-200 z-90 align-middle"
+                      src={params.row.image}
+                      alt="avatar"
+                    />
+                    <AvatarFallback>
+                      <User className="w-12 h-12" />
+                    </AvatarFallback>
+                  </Avatar>
+
+                  <div className="flex flex-col items-center">
+                    <h3 className="font-medium text-xl dark:text-white ">
+                      {params.row.fullName}
+                    </h3>
+                    <h4 className="text-[#737373] dark:text-slate-500">
+                      @{params.row.username}
+                    </h4>
+                  </div>
+                  <div className="flex flex-col w-full overflow-hidden gap-2 max-w-sm items-start px-2">
+                    <div className="w-full flex flex-col gap-1">
+                      <Separator className="w-8 self-center my-2 bg-border-color dark:bg-border-color-dark" />
+                      <h5 className="text-center w-full font-medium dark:text-white">
+                        Datos personales
+                      </h5>
+
+                      <ul className="flex flex-col w-full overflow-hidden gap-1 shadow-md py-2 rounded-md bg-white border border-border-color px-1 lg:px-2 dark:border-border-color-dark dark:bg-black">
+                        <li className="flex items-center gap-1">
+                          <Mail className="h-4 w-4 text-icon-color shrink-0 dark:text-icon-color-dark" />
+                          <span className="font-medium">Email:</span>
+                          {params.row.email}
                         </li>
                         <li className="flex items-center gap-1">
-                          <Phone className="h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
+                          <Phone className="h-4 w-4 text-icon-color dark:text-icon-color-dark" />
                           <span className="font-medium">Celular:</span>{" "}
                           {params.row.phone}
                         </li>
                         <li className="flex items-center gap-1 shrink-0">
-                          <ContactIcon className="w-4 h-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300 shrink-0" />
+                          <Fingerprint className="w-4 h-4 text-icon-color dark:text-icon-color-dark shrink-0" />
                           <span className="font-medium shrink-0">DNI:</span>
                           <span className="shrink-0">{params.row.dni}</span>
                         </li>
-                        <li className="flex items-center gap-1">
-                          <Mail className="h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                          <span className="font-medium">Email:</span>
-                          {params.row.email}
-                        </li>
                       </ul>
                     </div>
+
+                    <div className="w-full flex flex-col gap-1 ">
+                      <Separator className="w-8 self-center my-2 bg-border-color dark:bg-border-color-dark " />
+                      <h5 className="text-center w-full font-medium dark:text-white">
+                        Domicilios
+                      </h5>
+                      <div className="flex flex-col gap-1 shadow-md py-2 rounded-md bg-white border border-border-color px-1 lg:px-2 dark:border-border-color-dark dark:bg-black">
+                        <div className="flex flex-col gap-1">
+                          <h6 className="font-serif text-icon-color dark:text-icon-color-dark">
+                            Carmen de Areco
+                          </h6>
+                          <div className="flex items-center gap-1">
+                            <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                            <span className="font-medium dark:text-white">
+                              Dirreción:
+                            </span>
+                            <p>{`${params.row.addressCda.street} ${params.row.addressCda.streetNumber}`}</p>
+                          </div>
+                          <div className="flex flex-col gap-[2px]">
+                            <div className="flex items-center gap-1">
+                              <Crop className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                              <span className="font-medium dark:text-white">
+                                Calles que cruzan:
+                              </span>
+                            </div>
+                            <span className="">
+                              {params.row.addressCda.crossStreets}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="flex flex-col gap-1 lg:basis-[40%]">
+                          <h6 className="font-serif text-icon-color dark:text-icon-color-dark">
+                            Capital Federal
+                          </h6>
+                          <div className="flex items-center gap-1">
+                            <Milestone className="w-4 h-4 text-icon-color dark:text-icon-color-dark" />
+                            <span className="font-medium dark:text-white">
+                              Dirreción:
+                            </span>{" "}
+                            <p>{params.row.addressCapital}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
+
                   <div
-                    className="flex justify-center my-4 lg:w-[11rem] mx-auto"
+                    className="flex justify-center my-4 mx-auto"
                     onClick={() => handleAddPassenger(params.row._id)}
                   >
                     <DefaultButton loading={loading}>
