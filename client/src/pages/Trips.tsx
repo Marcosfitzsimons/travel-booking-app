3822b58c84e99836defb6c0696dd4f898cbcaa67
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-14 15:01:20 -0300

    Increase gap between cards

--- client/src/pages/Trips.tsx
+++ client/src/pages/Trips.tsx
@@ -108,7 +108,7 @@ const Trips = () => {
           animate="visible"
           exit="exit"
         >
-          <div className="mt-8 flex flex-col items-center gap-6 md:grid md:justify-items-center md:grid-cols-2 xl:grid-cols-3">
+          <div className="mt-8 flex flex-col items-center gap-8 md:grid md:justify-items-center md:grid-cols-2 xl:grid-cols-3">
             {filteredTrips ? (
               <>
                 <AnimatePresence mode="wait">
