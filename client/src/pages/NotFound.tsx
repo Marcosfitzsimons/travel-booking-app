7449fbb4c6d1c5e1be3a5e49c2c25d984b79d3e1
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-15 11:10:29 -0300

    Update notfound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import BackButton from "@/components/BackButton";
+import { Frown } from "lucide-react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>This page doesn't exists. :(</h1>
-    </div>
+    <section className="min-h-screen pt-6 flex flex-col items-center gap-3">
+      <div className="flex flex-col items-center">
+        <p className="text-xl">Ooops!</p>
+        <div className="flex items-center gap-1">
+          <h1>La página que estás buscando no existe</h1>
+          <Frown className="w-5 h-5" />
+        </div>
+      </div>
+      <BackButton toProfile={false} />
+    </section>
   );
 };
 
