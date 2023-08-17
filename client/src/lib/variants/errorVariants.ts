434e6738634006cb793e262758718a90c72f95a6
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-17 10:35:04 -0300

    Add errorVariants

---
+++ client/src/lib/variants/errorVariants.ts
@@ -0,0 +1,16 @@
+const errorVariants = {
+    hidden: {
+      opacity: 0,
+      y: 15,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+        ease: "easeIn",
+      },
+    },
+  };
+  
+  export default errorVariants
