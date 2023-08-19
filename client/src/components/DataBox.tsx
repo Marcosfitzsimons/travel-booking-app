cffb35679aa444bcbace367689a924ee4ae5eb70
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-19 10:23:01 -0300

    Add databox component

---
+++ client/src/components/DataBox.tsx
@@ -0,0 +1,19 @@
+import { DataBoxProps } from "@/types/props";
+import { Separator } from "./ui/separator";
+
+const DataBox = ({ icon, text, children }: DataBoxProps) => {
+  return (
+    <li className="flex flex-row items-center gap-3 py-1">
+      {icon}
+      <div className="w-full flex flex-col">
+        <span className="relative top-0.5 text-sm font-medium text-card-foreground">
+          {text}
+        </span>
+        <span>{children}</span>
+        <Separator className="mt-2" />
+      </div>
+    </li>
+  );
+};
+
+export default DataBox;
