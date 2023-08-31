00ff01eafcbc01e9f4631e6e8a5cb26988505087
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-31 13:57:29 -0300

    Change border radius to back button

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -7,10 +7,10 @@ type ButtonProps = {
 
 const BackButton = ({ linkTo }: ButtonProps) => {
   return (
-    <div className="flex items-center relative after:rounded-full after:absolute after:pointer-events-none after:inset-px after:shadow-highlight after:shadow-slate-200/20 after:transition focus-within:after:shadow-slate-400 dark:after:shadow-highlight dark:after:shadow-zinc-500/50 dark:focus-within:after:shadow-slate-100 dark:hover:text-white">
+    <div className="flex items-center relative after:rounded-[7px] after:absolute after:pointer-events-none after:inset-px after:shadow-highlight after:shadow-slate-200/20 after:transition focus-within:after:shadow-slate-400 dark:after:shadow-highlight dark:after:shadow-zinc-500/50 dark:focus-within:after:shadow-slate-100 dark:hover:text-white">
       <Link
         to={linkTo}
-        className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-card border border-slate-800/20 hover:bg-white dark:text-neutral-200 dark:border-slate-800 dark:hover:bg-black dark:hover:text-white"
+        className="flex h-[34px] w-[34px] items-center justify-center rounded-lg bg-card border border-slate-800/20 hover:bg-white dark:text-neutral-200 dark:border-slate-800 dark:hover:bg-black dark:hover:text-white"
       >
         <ChevronLeft className="h-5 w-5 shrink-0 text-secondary-foreground" />
       </Link>
