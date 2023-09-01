8612f900126da1f65bb1c1368eb9890baf2d8292
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-01 13:02:13 -0300

    Add useLogOut hook

---
+++ client/src/hooks/useLogOut.ts
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import axios from "../api/axios";
+import useAuth from "./useAuth";
+
+const useLogout = () => {
+    const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+
+
+    const logout = async () => {
+        try {
+            await axios('auth/logout', {
+                withCredentials: true
+        });
+        setAuth({ user: null });
+        navigate("/login");
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return logout;
+}
+
+export default useLogout
