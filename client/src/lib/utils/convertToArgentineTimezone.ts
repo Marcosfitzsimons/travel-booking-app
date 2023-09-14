6d8d402751a584ffa32fe2c2516ab56a28e1c3bf
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-14 19:12:13 -0300

    Change convertToArgTimezone function

--- client/src/lib/utils/convertToArgentineTimezone.ts
+++ client/src/lib/utils/convertToArgentineTimezone.ts
@@ -3,8 +3,10 @@ import moment from "moment";
 export const convertToArgentineTimezone = (dateStr: string) => {
     const utcDate = moment.utc(dateStr);
 
+    const bsAsDate = utcDate.tz("America/Argentina/Buenos_Aires");
+    
     // Use moment.format to display the date in the desired format
-    const formattedDate = utcDate.format("ddd DD/MM - hh:mm A");
+    const formattedDate = bsAsDate.format("ddd DD/MM - hh:mm A");
 
     const datePart = formattedDate.slice(0, 10); // "Dom 16/07"
     const timePart = formattedDate.slice(12); // "04:20 PM"
